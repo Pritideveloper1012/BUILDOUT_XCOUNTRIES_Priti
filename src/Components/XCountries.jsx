b1ad3be0794f9a801b9a7daee81be0c73af45954
Priti Gupta <priticse2633@gmail.com>
2025-07-29 17:11:59 +0530

    Initial commit

---
+++ src/Components/XCountries.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import {TextField,Box,Container} from '@mui/material'
+import { useState } from "react";
+import { useEffect } from "react";
+
+const XCountries=()=>{
+    const [countries,setCountries]=useState([])
+    const [error,setError]=useState([])
+
+    useEffect(()=>{
+        
+
+    },[])
+
+}
+export default XCountries;
